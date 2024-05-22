518362f34250ee8f28a6aa08a3545b48e227c458
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-05-22 11:55:02 +0200

    refactor: enforce chains constants and types

--- src/chains/evm/common/utils/provider.ts
+++ src/chains/evm/common/utils/provider.ts
@@ -1,24 +1,28 @@
 import { createPublicClient, fallback, http } from "viem";
 
-import { FOLKS_CHAIN_ID } from "../../../../common/constants/chain.js";
-import { CHAIN_VIEM, CHAIN_NODE } from "../constants/chain.js";
+import {
+  CHAIN_VIEM,
+  CHAIN_NODE,
+  EVM_FOLKS_CHAIN_ID,
+  isEvmChainId,
+} from "../constants/chain.js";
 
 import type { FolksChainId } from "../../../../common/types/chain.js";
-import type { ChainId } from "../types/chain.js";
+import type { EvmChainId } from "../types/chain.js";
 import type { PublicClient } from "viem";
 
 export function initProviders(
   customProvider: Partial<Record<FolksChainId, PublicClient>>,
 ): Record<FolksChainId, PublicClient> {
   return Object.fromEntries(
-    Object.values(FOLKS_CHAIN_ID).map((folksChainId) => {
+    Object.values(EVM_FOLKS_CHAIN_ID).map((evmFolksChainId) => {
       return [
-        folksChainId,
-        customProvider[folksChainId] ??
+        evmFolksChainId,
+        customProvider[evmFolksChainId] ??
           createPublicClient({
-            chain: CHAIN_VIEM[folksChainId],
+            chain: CHAIN_VIEM[evmFolksChainId],
             transport: fallback(
-              CHAIN_NODE[folksChainId].map((url: string) => http(url)),
+              CHAIN_NODE[evmFolksChainId].map((url: string) => http(url)),
             ),
           }),
       ];
@@ -26,8 +30,12 @@ export function initProviders(
   ) as Record<FolksChainId, PublicClient>;
 }
 
-export function getChainId(provider: PublicClient): ChainId {
-  if (provider.chain?.id === undefined)
+export function getChainId(provider: PublicClient): EvmChainId {
+  const chainId = provider.chain?.id;
+  if (chainId === undefined)
     throw new Error("EVM provider chain id is undefined");
-  return provider.chain.id;
+  if (!isEvmChainId(chainId))
+    throw new Error(`EVM provider chain id is not supported: ${chainId}`);
+
+  return chainId;
 }
