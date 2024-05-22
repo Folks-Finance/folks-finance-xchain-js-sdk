30008ddb70b898e1e9a7e4e69958b7717f16a674
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-05-22 16:44:03 +0200

    chore: add exports from evm chains

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// CORE
+// === CORE ===
 export { FolksCore } from "./xchain/core/folks-core.js";
 
-// MODULES
+// === MODULES ===
 export { FolksAccount, FolksLoan } from "./xchain/modules/index.js";
 
-// COMMON
+// === COMMON ===
 export * from "./common/types/address.js";
 export * from "./common/types/chain.js";
 export * from "./common/types/core.js";
@@ -16,5 +16,11 @@ export * from "./common/constants/bytes.js";
 export * from "./common/constants/chain.js";
 export * from "./common/constants/message.js";
 
-// CHAINS
-export { CHAIN_VIEM } from "./chains/evm/common/constants/chain.js";
+// === CHAINS ===
+
+// - EVM
+export { CHAIN_VIEM, CHAIN_NODE } from "./chains/evm/common/constants/chain.js";
+
+export * from "./chains/evm/common/types/chain.js";
+
+export { isEvmChainId } from "./chains/evm/common/utils/chain.js";
