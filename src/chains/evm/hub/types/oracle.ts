b2ce1684eb6d902d31ebe087970123c1b922939f
gidonkatten <gidonkatten@gmail.com>
2024-07-17 19:52:06 +0100

    fix: use oracle token decimals rather than defined constant

--- src/chains/evm/hub/types/oracle.ts
+++ src/chains/evm/hub/types/oracle.ts
@@ -1,6 +1,9 @@
 import type { FolksTokenId } from "../../../../common/types/token.js";
 import type { Dnum } from "dnum";
 
-export type OraclePrice = Dnum;
+export type OraclePrice = {
+  price: Dnum;
+  decimals: number;
+};
 
 export type OraclePrices = Partial<Record<FolksTokenId, OraclePrice>>;
