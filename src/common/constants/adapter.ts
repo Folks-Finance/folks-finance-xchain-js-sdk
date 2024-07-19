265f679af06bd15f065b7239333322a1079939cf
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-07-19 17:18:26 +0200

    refactor: strengthen adapter type definitions

--- src/common/constants/adapter.ts
+++ src/common/constants/adapter.ts
@@ -1,5 +1,5 @@
 import { AdapterType } from "../types/message.js";
 
-export const TOKEN_ADAPTERS = [AdapterType.WORMHOLE_CCTP, AdapterType.CCIP_TOKEN];
-export const DATA_ADAPTERS = [AdapterType.WORMHOLE_DATA, AdapterType.CCIP_DATA];
-export const HUB_ADAPTERS = [AdapterType.HUB];
+export const TOKEN_ADAPTERS = [AdapterType.WORMHOLE_CCTP, AdapterType.CCIP_TOKEN] as const;
+export const DATA_ADAPTERS = [AdapterType.WORMHOLE_DATA, AdapterType.CCIP_DATA] as const;
+export const HUB_ADAPTERS = [AdapterType.HUB] as const;
