195603853e09eaa16facfb63894d39c46fc35760
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-09-04 09:37:51 +0100

    feat: export missing core modules

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export { FolksCore } from "./xchain/core/folks-core.js";
 
 // === MODULES ===
-export { FolksAccount, FolksLoan, FolksOracle, FolksPool } from "./xchain/modules/index.js";
+export { FolksAccount, FolksLoan, FolksOracle, FolksPool, FolksGmp, FolksRewards } from "./xchain/modules/index.js";
 
 // === COMMON ===
 export * from "./common/types/adapter.js";
