1c4d7548397260f2ad7b247a52c072011db8c898
Gidon Katten <55109236+gidonkatten@users.noreply.github.com>
2024-09-17 13:28:30 +0100

    feat: estimate message fee (#152)

---
+++ .changeset/tender-bobcats-accept.md
@@ -0,0 +1,5 @@
+---
+"@folks-finance/xchain-sdk": patch
+---
+
+estimate message fee
--- src/chains/evm/hub/modules/folks-hub-gmp.ts
+++ src/chains/evm/hub/modules/folks-hub-gmp.ts
@@ -1,13 +1,19 @@
+import { FINALITY } from "../../../../common/constants/message.js";
+import { getRandomGenericAddress } from "../../../../common/utils/address.js";
+import { getRandomBytes } from "../../../../common/utils/bytes.js";
 import { GAS_LIMIT_ESTIMATE_INCREASE } from "../../common/constants/contract.js";
 import { getEvmSignerAccount } from "../../common/utils/chain.js";
+import { buildMessageParams, buildSendTokenExtraArgsWhenRemoving } from "../../common/utils/message.js";
 import { getBridgeRouterHubContract } from "../utils/contract.js";
 
 import type { EvmAddress } from "../../../../common/types/address.js";
+import type { FolksChainId } from "../../../../common/types/chain.js";
 import type { MessageId } from "../../../../common/types/gmp.js";
-import type { AdapterType } from "../../../../common/types/message.js";
+import type { AdapterType, MessageToSend } from "../../../../common/types/message.js";
 import type { MessageReceived } from "../../common/types/gmp.js";
 import type { PrepareRetryMessageCall, PrepareReverseMessageCall } from "../../common/types/module.js";
 import type { HubChain } from "../types/chain.js";
+import type { HubTokenData } from "../types/token.js";
 import type { Client, EstimateGasParameters, Hex, WalletClient } from "viem";
 
 export const prepare = {
@@ -111,3 +117,34 @@ export const write = {
     });
   },
 };
+
+export async function getSendMessageFee(
+  provider: Client,
+  toFolksChainId: FolksChainId,
+  adapterId: AdapterType,
+  gasLimit: bigint,
+  hubChain: HubChain,
+  hubTokenData: HubTokenData,
+): Promise<bigint> {
+  const hubBridgeRouter = getBridgeRouterHubContract(provider, hubChain.bridgeRouterAddress);
+
+  // construct return message
+  const message: MessageToSend = {
+    params: buildMessageParams({
+      adapters: {
+        adapterId,
+        returnAdapterId: 0 as AdapterType,
+      },
+      gasLimit,
+    }),
+    sender: hubChain.hubAddress,
+    destinationChainId: toFolksChainId,
+    handler: getRandomGenericAddress(),
+    payload: getRandomBytes(256),
+    finalityLevel: FINALITY.FINALISED,
+    extraArgs: buildSendTokenExtraArgsWhenRemoving(getRandomGenericAddress(), hubTokenData.token, 1n),
+  };
+
+  // get return adapter fee
+  return await hubBridgeRouter.read.getSendFee([message]);
+}
