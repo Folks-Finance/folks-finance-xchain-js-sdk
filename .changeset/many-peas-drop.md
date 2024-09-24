2ed67dd33312621cd31088e2ee893c28b8e97e54
Giuseppe Palazzolo <38705839+palace22@users.noreply.github.com>
2024-09-24 11:46:05 +0200

    fix: retry and reverse gas limit estimation (#174)
    Added increase by 5% in retry and reverse gas limit estimation

---
+++ .changeset/many-peas-drop.md
@@ -0,0 +1,5 @@
+---
+"@folks-finance/xchain-sdk": patch
+---
+
+Added increase by 5% in retry and reverse gas limit estimation
--- src/chains/evm/common/constants/contract.ts
+++ src/chains/evm/common/constants/contract.ts
@@ -9,3 +9,4 @@ export const defaultEventParams: EventParams = {
 export const GAS_LIMIT_ESTIMATE_INCREASE = 10_000n;
 export const SEND_TOKEN_ACTION_RETURN_GAS_LIMIT = 500_000n;
 export const RECEIVER_VALUE_SLIPPAGE = 0.01;
+export const RETRY_REVERSE_GAS_LIMIT_SLIPPAGE = 0.05;
