ec7b4df529706683041cd29fcd69734327764586
palace22 <palazzolo1995@gmail.com>
2024-10-15 19:27:48 +0200

    refactor: gmp utils

--- src/common/utils/gmp.ts
+++ src/common/utils/gmp.ts
@@ -4,13 +4,9 @@ import type { FolksChainId } from "../types/chain.js";
 import type { CCIPData, WormholeData } from "../types/gmp.js";
 
 export function getWormholeData(folksChainId: FolksChainId): WormholeData {
-  const wormholeData = WORMHOLE_DATA[folksChainId];
-  if (wormholeData) return wormholeData;
-  throw new Error(`Wormhole data not found for folksChainId: ${folksChainId}`);
+  return WORMHOLE_DATA[folksChainId];
 }
 
 export function getCcipData(folksChainId: FolksChainId): CCIPData {
-  const ccipData = CCIP_DATA[folksChainId];
-  if (ccipData) return ccipData;
-  throw new Error(`CCIP data not found for folksChainId: ${folksChainId}`);
+  return CCIP_DATA[folksChainId];
 }
