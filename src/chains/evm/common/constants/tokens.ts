1f52a32c925b7d881997b8bffeda5537de713788
palace22 <palazzolo1995@gmail.com>
2024-11-04 19:49:04 +0100

    feat: arbitrum

--- src/chains/evm/common/constants/tokens.ts
+++ src/chains/evm/common/constants/tokens.ts
@@ -81,6 +81,22 @@ export const CONTRACT_SLOT: Partial<
       },
     },
   },
+  [EVM_FOLKS_CHAIN_ID.ARBITRUM]: {
+    erc20: {
+      [MAINNET_FOLKS_TOKEN_ID.USDC]: {
+        balanceOf: 9n,
+        allowance: 10n,
+      },
+      [MAINNET_FOLKS_TOKEN_ID.ARB]: {
+        balanceOf: 51n,
+        allowance: 52n,
+      },
+      [MAINNET_FOLKS_TOKEN_ID.SolvBTC]: {
+        balanceOf: BigInt("37439836327923360225337895871394760624280537466773280374265222508165906222592"),
+        allowance: BigInt("37439836327923360225337895871394760624280537466773280374265222508165906222593"),
+      },
+    },
+  },
   [EVM_FOLKS_CHAIN_ID.AVALANCHE_FUJI]: {
     erc20: {
       [TESTNET_FOLKS_TOKEN_ID.USDC]: {
