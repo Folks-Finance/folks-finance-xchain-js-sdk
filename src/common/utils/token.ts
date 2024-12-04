91a1daa880a8d76cccbebae2cffa91dd26abb0a7
palace22 <palazzolo1995@gmail.com>
2024-12-04 17:40:04 +0100

    fix: folks token id from pool const

--- src/common/utils/token.ts
+++ src/common/utils/token.ts
@@ -1,14 +1,15 @@
-import { FOLKS_TOKEN_IDS_FROM_POOL } from "../constants/pool.js";
+import { FOLKS_TOKEN_IDS_FROM_POOL_BY_NETWORK } from "../constants/pool.js";
 import { CROSS_CHAIN_FOLKS_TOKEN_ID } from "../constants/token.js";
 
+import type { NetworkType } from "../types/chain.js";
 import type { FolksTokenId } from "../types/token.js";
 
 export function isCrossChainToken(folksTokenId: FolksTokenId): boolean {
   return CROSS_CHAIN_FOLKS_TOKEN_ID.includes(folksTokenId);
 }
 
-export function getFolksTokenIdFromPool(poolId: number): FolksTokenId {
-  const folksTokenId = FOLKS_TOKEN_IDS_FROM_POOL[poolId];
+export function getFolksTokenIdFromPool(poolId: number, network: NetworkType): FolksTokenId {
+  const folksTokenId = FOLKS_TOKEN_IDS_FROM_POOL_BY_NETWORK[network][poolId];
   if (!folksTokenId) throw new Error(`Unknown poolId: ${poolId}`);
   return folksTokenId;
 }
